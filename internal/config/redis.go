3502ff2ae13dcf16c9edba3fe530e68e8032635c
sina shariati <sina.shariati@yahoo.com>
2023-11-06 14:03:46 +0330

    Add timeouts to configs/redis.go

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -1,9 +1,14 @@
 package config
 
+import "time"
+
 type redis struct {
-	Address  string `json:"address,omitempty" default:"address"`
-	Password string `json:"password,omitempty" default:"password"`
-	DB       int    `json:"db,omitempty" default:"db"`
+	Address      string        `json:"address,omitempty" default:"address"`
+	Password     string        `json:"password,omitempty" default:"password"`
+	DB           int           `json:"db,omitempty" default:"db"`
+	DialTimeout  time.Duration `json:"dial_timeout,omitempty" default:"dial_timeout"`
+	ReadTimeout  time.Duration `json:"read_timeout,omitempty" default:"read_timeout"`
+	WriteTimeout time.Duration `json:"write_timeout,omitempty" default:"write_timeout"`
 }
 
 var Redis *redis
