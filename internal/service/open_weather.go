5e65af4552f742f897407fbb5d2b176d0f3ea86c
sina shariati <sina.shariati@yahoo.com>
2023-11-06 17:50:39 +0330

    Finish GetWeatherStatusByCity()

--- internal/service/open_weather.go
+++ internal/service/open_weather.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/noisyboy-9/golang_api_template/internal/config"
 	"github.com/noisyboy-9/golang_api_template/internal/model"
 )
@@ -17,4 +21,16 @@ func InitOpenWeatherConnection() {
 }
 
 func (ow *openWeather) GetWeatherByCityName(city string) (*model.WeatherStatus, error) {
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, ow.ApiKey)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	status := new(model.WeatherStatus)
+	if err := json.NewDecoder(response.Body).Decode(status); err != nil {
+		return nil, err
+	}
+
+	return status, nil
 }
